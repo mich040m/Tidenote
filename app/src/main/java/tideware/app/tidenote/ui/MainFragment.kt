0f1b9d4dc270dc611fb16c3e0ad82557616ccba2
Michal Tomasz Odzienkowski <mich040m@unv.eucsj.dk>
2020-08-06 18:49:33 +0200

    Initial commit

---
+++ app/src/main/java/tideware/app/tidenote/ui/MainFragment.kt
@@ -0,0 +1,38 @@
+package tideware.app.tidenote.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_main.*
+import tideware.app.tidenote.R
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        fab_main.setOnClickListener{
+            findNavController().navigate(R.id.action_MainFragment_to_CreateEditFragment)
+        }
+    }
+}
